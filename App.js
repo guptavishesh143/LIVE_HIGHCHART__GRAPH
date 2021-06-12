d0fdd42cc8a53104d3903b625f83b894ed69d3ed
guptavishesh143 <ait.17bcs4133@gmail.com>
2021-06-12 19:08:53 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import HighchartsReactNative from '@highcharts/highcharts-react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chartOptions: {
+        chart: {
+          events: {
+            load: function () {
+              // set up the updating of the chart each second
+              var series = this.series[0];
+              setInterval(function () {
+                var y = Math.random();
+                series.addPoint(y, true, true);
+              }, 1000);
+            },
+          },
+        },
+        series: [
+          {
+            data: [1, 2, 3],
+          },
+        ],
+      },
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <HighchartsReactNative
+          styles={styles.container}
+          options={this.state.chartOptions}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
